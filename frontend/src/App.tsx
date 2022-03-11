36176042cdb339b260a461e0e9a008ae5ed87175
ashrabino <rasheen.rabino-20@cpu.edu.ph>
2022-03-11 15:02:00 +0800

    added alert system

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 import './App.css';
 import  { Schedule, ResourcesDirective, ResourceDirective, DragAndDrop, Resize, Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda,  ViewsDirective, ViewDirective } from '@syncfusion/ej2-react-schedule';
+import { extend } from '@syncfusion/ej2-base';
+
 
 export default class App extends React.Component <{}, {}> {
+  scheduleObj: any;
+
+  constructor() {
+    super({});
+  }
+
 
   private localData :  object[]  = [
     {
@@ -19,6 +27,7 @@ export default class App extends React.Component <{}, {}> {
       StartTime: new Date(2022, 3, 11, 7, 0),
       EndTime: new Date(2022, 3, 11, 10, 0),
       ResourceID:1,
+      allowAdding: false
     }
   ]
 
@@ -27,37 +36,57 @@ export default class App extends React.Component <{}, {}> {
     {Name: 'Second Subterm', Id: 2, Color: '#357CD2', IsBlock: true}
   ]
 
+  onActionBegin(args: { requestType: string; data: string | any[]; cancel: boolean; }) {
+    if (args.requestType === 'eventCreate' && args.data.length > 0) {
+        let eventData = args.data[0];
+        let eventField = this.scheduleObj.eventFields;
+        let startDate = eventData[eventField.startTime];
+        let endDate = eventData[eventField.endTime];
+  
+        if(!this.scheduleObj.isSlotAvailable(startDate, endDate)) {
+          args.cancel = true;
+          alert('Conflicting timeslot!')
+        }
+  }
+  }
+
   public render() {
     return (
-    <ScheduleComponent  
-    currentView='Week'
-    selectedDate={new Date(2022, 3, 11)}
-    showHeaderBar={false}
-    allowDragAndDrop={true}
-    allowMultiCellSelection={true} 
-    allowMultiDrag={true} allowResizing={true}
-    eventSettings={{ dataSource: this.localData,
-      fields: {
-          id: 'Id',
-          subject: { name: 'Subject', title: 'Subject' },
-          location: { name: 'Location', title: 'Location'},
-          description: { name: 'Description', title: 'Class Description' },
-          startTime: { name: 'StartTime', title: 'From' },
-          endTime: { name: 'EndTime', title: 'To' },
-      }
-  }}>
+
+      <ScheduleComponent
+
+        ref={(schedule: any) => this.scheduleObj = schedule}
+        width='100%'  
+        currentView='Week'
+        showHeaderBar={false}
+        allowDragAndDrop={true}
+        allowMultiCellSelection={true}
+        allowMultiDrag={true} 
+        allowResizing={true}
+        eventSettings={{
+          dataSource: this.localData,
+          fields: {
+            id: 'Id',
+            subject: { name: 'Subject', title: 'Subject' },
+            location: { name: 'Location', title: 'Location' },
+            description: { name: 'Description', title: 'Class Description' },
+            startTime: { name: 'StartTime', title: 'From' },
+            endTime: { name: 'EndTime', title: 'To' },
+          }
+        }}
+        actionBegin={this.onActionBegin.bind(this)}>
+
+
+          <ViewsDirective>
+            <ViewDirective option='Week' startHour='7:00' endHour='21:00' firstDayOfWeek={1}> </ViewDirective>
+          </ViewsDirective>
+          <ResourcesDirective>
+            <ResourceDirective field='ResourceID' title='Subterm' name='Resources' textField='Name' idField='Id' colorField='Color' dataSource={this.resourceDataSource}></ResourceDirective>
+          </ResourcesDirective>
+
+          <Inject services={[Day, Week, WorkWeek, Month, Agenda, DragAndDrop, Resize, Schedule]} />
+        </ScheduleComponent>
     
-
-      <ViewsDirective>
-        <ViewDirective option = 'Week' startHour='7:00' endHour='21:00' firstDayOfWeek={1}> </ViewDirective>
-      </ViewsDirective>
-      <ResourcesDirective>
-        <ResourceDirective field='ResourceID' title='Subterm' name='Resources' textField='Name' idField='Id' colorField='Color' dataSource={this.resourceDataSource}></ResourceDirective>
-      </ResourcesDirective>
-      
-      <Inject services={[ Day, Week, WorkWeek, Month, Agenda, DragAndDrop, Resize, Schedule]} />
-    </ScheduleComponent>
-
     
     
   );
